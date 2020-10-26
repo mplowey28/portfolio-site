48f09092dc79de2c16bc657753f0297883e0b7d8
mplowey28 <matthew.plowey@gmail.com>
2020-10-26 20:19:25 +0100

    added projects section

---
+++ src/components/Projects/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <ProjectsContainer id="projects">
+            <ProjectsH1>Projects</ProjectsH1>
+            <ProjectWrapper>
+                <ProjectsCard>
+                    <ProjectsImg />
+                </ProjectsCard>
+            </ProjectWrapper>
+        </ProjectsContainer>
+    )
+}
+
+export default Projects
