3a1dc40de56ab9dff62a9b7da671842089dbd69c
mplowey28 <matthew.plowey@gmail.com>
2022-01-14 20:38:23 +0100

    Remove bad slider

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,51 +1,7 @@
-import React, { useState } from "react";
-import { sliderData } from "./content";
-import {
-  SliderContainer,
-  SlideContainer,
-  RightIcon,
-  LeftIcon,
-  Img,
-  Video,
-} from "./SliderElements";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import React from "react";
 
 const Slider = () => {
-  const [current, setCurrent] = useState(0);
-  const length = sliderData.length;
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
-  return (
-    <SliderContainer id="projects">
-      <LeftIcon>
-        <IoIosArrowBack onClick={prevSlide} />
-      </LeftIcon>
-      <RightIcon>
-        <IoIosArrowForward onClick={nextSlide} />
-      </RightIcon>
-      {sliderData.map((slide, index) => {
-        return (
-          <SlideContainer
-            key={index}
-            current={index === current ? "slide active" : "slide"}
-          >
-            {index === current &&
-              (slide.type === "img" ? (
-                <Img src={slide.image} alt={slide.title} />
-              ) : (
-                <Video autoPlay loop muted src={slide.image} type="video/mp4" />
-              ))}
-          </SlideContainer>
-        );
-      })}
-    </SliderContainer>
-  );
+  return <div></div>;
 };
 
 export default Slider;
