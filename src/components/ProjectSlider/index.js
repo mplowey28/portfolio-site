92466cc651404ad5b7e510b650dc5ee60fef581b
mplowey28 <matthew.plowey@gmail.com>
2022-02-14 16:33:51 +0100

    add Alice carousel

--- src/components/ProjectSlider/index.js
+++ src/components/ProjectSlider/index.js
@@ -1,68 +1,62 @@
-import React from "react";
-import { sliderData } from "./content";
+import React from 'react'
+import { sliderData } from './content'
 import {
   SliderContainer,
   Heading,
-  ProjectCardsWrapper,
   ProjectCard,
   ProjectImgLink,
   LinkContainer,
   LiveDemoContainer,
   ProjectTitle,
   ProjectDescription,
-} from "./ProjectSliderElements";
-import { FaGithub, FaPlay } from "react-icons/fa";
+} from './ProjectSliderElements'
+import { FaGithub, FaPlay } from 'react-icons/fa'
+import AliceCarousel from 'react-alice-carousel'
+
+const slides = sliderData.map((card) => {
+  return (
+    <ProjectCard key={card.title}>
+      <ProjectTitle>{card.title}</ProjectTitle>
+      <ProjectImgLink>
+        {card.type === 'img' ? (
+          <img src={card.image} alt={card.title} />
+        ) : (
+          <video autoPlay loop muted src={card.image} type='video/mp4' />
+        )}
+      </ProjectImgLink>
+      <ProjectDescription>{card.desc}</ProjectDescription>
+      <LinkContainer>
+        <LiveDemoContainer
+          href={card.github}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaGithub />
+          Code
+        </LiveDemoContainer>
+
+        {card?.demo && (
+          <LiveDemoContainer
+            href={card.demo}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaPlay />
+            Demo
+          </LiveDemoContainer>
+        )}
+      </LinkContainer>
+    </ProjectCard>
+  )
+})
 
 const ProjectSlider = () => {
   return (
-    <SliderContainer id="projects">
+    <SliderContainer id='projects'>
       <Heading>Projects</Heading>
-      <ProjectCardsWrapper>
-        {sliderData.map((card) => {
-          return (
-            <ProjectCard key={card.title}>
-              <ProjectTitle>{card.title}</ProjectTitle>
-              <ProjectImgLink>
-                {card.type === "img" ? (
-                  <img src={card.image} alt={card.title} />
-                ) : (
-                  <video
-                    autoPlay
-                    loop
-                    muted
-                    src={card.image}
-                    type="video/mp4"
-                  />
-                )}
-              </ProjectImgLink>
-              <ProjectDescription>{card.desc}</ProjectDescription>
-              <LinkContainer>
-                <LiveDemoContainer
-                  href={card.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaGithub />
-                  Code
-                </LiveDemoContainer>
-
-                {card?.demo && (
-                  <LiveDemoContainer
-                    href={card.demo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaPlay />
-                    Demo
-                  </LiveDemoContainer>
-                )}
-              </LinkContainer>
-            </ProjectCard>
-          );
-        })}
-      </ProjectCardsWrapper>
+      <AliceCarousel items={slides} />
     </SliderContainer>
-  );
-};
+  )
+}
 
-export default ProjectSlider;
+export default ProjectSlider
